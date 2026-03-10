e88b47788a66eec7c7daca3c6fc0bca560d57292
Yiroma <yiromaric@gmail.com>
2026-03-10 12:20:52 +0100

    feat(home): ajoute la section 'projets' à la page Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/home/Hero";
 import { ServicesPreview } from "@/components/home/ServicesPreview";
 import { WhyYiroma } from "@/components/home/WhyYiroma";
+import { ProjectsPreview } from "@/components/home/ProjectsPreview";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <ServicesPreview />
       <WhyYiroma />
+      <ProjectsPreview />
     </main>
   );
 }
